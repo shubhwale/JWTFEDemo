610493ad974b2a95b5b079bba59a3e43c0ff398e
Shubham Kawale <shubhamkawale@gmail.com>
2020-07-27 15:05:13 +0530

    initial commit 3

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  cartItems = [
+    {bookId: 101, title: "abc", imageUrl: "assets/images/java8_in_action.jpeg",
+    rating: 4, quantity: 1, price: 300},
+    {bookId: 102, title: "abc", imageUrl: "assets/images/head_first_servlet_&_jsp.jpg",
+    rating: 4, quantity: 1, price: 300}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
